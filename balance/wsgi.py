5f7be5a63babf27d8d1788a8a61c77077dbc6969
promisenmor <nmorchibogu98@gmail.com>
2026-04-20 22:57:47 +0100

    restarted this project

--- balance/wsgi.py
+++ balance/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for balance_backend project.
+WSGI config for balance project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'balance_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'balance.settings')
 
 application = get_wsgi_application()
--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
+    
+
+class Transaction(models.Model):
+    TRANSACTION_TYPE = (
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=50, choices=TRANSACTION_TYPE)
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.transaction_type} - {self.amount}"
+    
+
+
+class Goal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    target_amount = models.DecimalField(max_digits=100, decimal_places=2)
+    current_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
