59fbd91a64f62b387407d274c4f2d264aa309202
promisenmor <nmorchibogu98@gmail.com>
2026-04-21 22:58:09 +0100

    started transactions

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, MeView
+from .views import RegisterView, MeView, TransactionDetailView, TransactionListCreateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -9,4 +9,7 @@ urlpatterns = [
     path("auth/login/", TokenObtainPairView.as_view()),
     path("auth/refresh/", TokenRefreshView.as_view()),
     path("auth/me/", MeView.as_view(), name="Profile"),
+    path("transactions/", TransactionListCreateView.as_view()),
+    path("transactions/<int:pk>", TransactionDetailView.as_view()),
 ]
+
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from .models import Transaction, Goal
@@ -25,4 +25,25 @@ class MeView(GenericAPIView):
 
     def get(self, request):
         return Response(UserSerializer(request.user).data)
+
+#List and create
+class TransactionListCreateView(ListCreateAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user).order_by('-created_at')
     
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
+
+# retrieve, update and delete
+class TransactionDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+    
+
